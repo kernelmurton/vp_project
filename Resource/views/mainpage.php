e71695f57bdf55dc7bd5476222c3fae268b0393a
kernelmurton <zetman.19981207@gmail.com>
2022-04-18 13:16:50 +0900

    pdoの実装

--- Resource/views/mainpage.php
+++ Resource/views/mainpage.php
@@ -1,3 +1,4 @@
+//セッションを回す
 <?php
 $session_time = 86400;
 ini_set('session.gc_divisor', 1);
@@ -5,7 +6,7 @@ ini_set('session.gc_maxlifetime', $session_time );
 
 session_start();
 if(isset($_SESSION['user_id'])==false){
-	header('Location:http://home.nda.ac.jp/~s66129//login/login.php');
+	header('Location:./login.php');
 	exit();
 }
 $user_id = $_SESSION['user_id'];
@@ -13,7 +14,6 @@ $user_id = $_SESSION['user_id'];
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
     <title>mainpage</title>
@@ -28,7 +28,24 @@ $user_id = $_SESSION['user_id'];
 
         </div>
         <div class="main objects">
-            
+            <?php
+            try{
+                require_once('../model/db.phpdb.php');
+                $pdo = new PDO($dsn, $user, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql="select * from Blogtable where id =:c_id";
+                $stmt=$pdo->prepare($sql);
+                $stmt->bindValue(":c_id", $contents_id, PDO::PARAM_STR);
+                $stmt->execute();
+                $title_rows= $stmt->fetchAll();
+                $title_row= $title_rows[0];
+                //var_dump($title_rows);
+                //var_dump($title_row);
+            }catch (PDOException $e){
+                print('error:'.$e->getMessage());
+                die();//エラーをはくようにする
+            }
+            ?>
             <section class="item">
                 <a href="">
                     <div class="thumbnail"></div>
